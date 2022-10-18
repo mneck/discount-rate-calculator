cd4633ab1715d4b74ca3dcb009919ccbc5a52ac2
mneck <matthew@climatelabs.de>
2022-10-18 14:47:34 +0200

    Adding present values, yrs 1-20

---
+++ present-value-table.js
@@ -0,0 +1,87 @@
+const cards = [
+  {
+    year: 0,
+    value: 0,
+  },
+  {
+    year: 1,
+    value: 0.9852,
+  },
+  {
+    year: 2,
+    value: 1.9559,
+  },
+  {
+    year: 3,
+    value: 2.9122,
+  },
+  {
+    year: 4,
+    value: 3.8544,
+  },
+  {
+    year: 5,
+    value: 4.7826,
+  },
+  {
+    year: 6,
+    value: 5.6972,
+  },
+  {
+    year: 7,
+    value: 6.5982,
+  },
+  {
+    year: 8,
+    value: 7.4859,
+  },
+  {
+    year: 9,
+    value: 8.3605,
+  },
+  {
+    year: 10,
+    value: 9.2222,
+  },
+  {
+    year: 11,
+    value: 10.0711,
+  },
+  {
+    year: 12,
+    value: 10.9075,
+  },
+  {
+    year: 13,
+    value: 11.7315,
+  },
+  {
+    year: 14,
+    value: 12.5434,
+  },
+  {
+    year: 15,
+    value: 13.3432,
+  },
+
+  {
+    year: 16,
+    value: 14.1313,
+  },
+  {
+    year: 17,
+    value: 14.9076,
+  },
+  {
+    year: 18,
+    value: 15.6726,
+  },
+  {
+    year: 19,
+    value: 16.4262,
+  },
+  {
+    year: 20,
+    value: 17.1686,
+  },
+];
